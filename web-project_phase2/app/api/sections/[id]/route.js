91785bd3add534a86d83b63fc364d2096a0fb1d4
aiman-aa2104619 <aa2104619@qu.edu.qa>
2025-08-28 11:43:50 +0300

    cancel

--- web-project_phase2/app/api/sections/[id]/route.js
+++
@@ -1,8 +0,0 @@
-import repo from "../../../repo/database-repo";
-
-export async function PUT(request, { params }) {
-     const sectionId = params.id;
-    const section = await request.json();
-    const updatedSection = await repo.updateSection(sectionId, section);
-    return Response.json(updatedSection);
-}
--- web-project_phase2/app/api/sections/[id]/students/route.js
+++
@@ -1,7 +0,0 @@
-import repo from "../../../../repo/database-repo";
-
-export async function GET(request, { params }) {
-    const sectionId = params.id;
-    const students = await repo.getStudentsBySectionId(sectionId)
-    return Response.json(students, { status: 200 });
-}
